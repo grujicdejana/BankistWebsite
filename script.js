a232ac391336e878da9543ea77778207ebdc1318
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-06-01 16:31:06 +0200

    Implementing page navigation by using event delegation

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -33,9 +35,6 @@ document.addEventListener('keydown', function (e) {
 });
 
 //SMOOTH SCROLLING
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function(e){
 	const s1coords = section1.getBoundingClientRect();
 	console.log(s1coords);
@@ -47,4 +46,15 @@ btnScrollTo.addEventListener('click', function(e){
   });
 	
 	//section1.scrollIntoView({ behavior: 'smooth'});
+});
+
+//Page navigation
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
